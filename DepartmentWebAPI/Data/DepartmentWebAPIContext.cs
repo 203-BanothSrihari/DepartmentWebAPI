26e02eed710be25cd5cd8c847c01b20f3fa24ea3
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 16:59:11 +0530

    Add project files.

---
+++ DepartmentWebAPI/Data/DepartmentWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DepartmentWebAPI.Model;
+
+namespace DepartmentWebAPI.Data
+{
+    public class DepartmentWebAPIContext : DbContext
+    {
+        public DepartmentWebAPIContext (DbContextOptions<DepartmentWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DepartmentWebAPI.Model.Department> Department { get; set; }
+    }
+}
---
+++ DepartmentWebAPI/Model/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepartmentWebAPI.Model
+{
+    public class Department
+    {
+        public int ID { get; set; }
+
+        public string DeptName { get; set; }
+
+        public string Course { get; set; }
+
+        public string Loation { get; set; }
+
+    }
+}
